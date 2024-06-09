ad85897b76039cd2d5ea46d231772721effc171d
Alex Perry <alexp3y@gmail.com>
2024-06-09 19:36:48 +0200

    Prevent mobile overscroll in CF

--- src/components/Colorflu/ColorfluPauseMenu.tsx
+++ src/components/Colorflu/ColorfluPauseMenu.tsx
@@ -26,7 +26,7 @@ const ColorfluPauseMenu: React.FC<Props> = ({ engine, resume }) => {
       className="w-full h-full flex items-center justify-center"
       onClick={() => resume()}
     >
-      <div className="bg-p3y-blue w-60 h-60 left-0 flex justify-center items-center">
+      <div className="border-p3y-gunmental border-2 rounded-3xl text-p3y-gunmetal w-60 h-60 left-0 flex justify-center items-center anta text-4xl bg-p3y-grey bg-opacity-30">
         PAUSED
       </div>
     </div>
